2e6f5b6c5555a9c8ab0d0559af9dd59844dee8c6
Sergio Quijano <sergiquijano@gmail.com>
2021-09-12 18:22:20 +0200

    feat: executing commands now returns error ocurred during execution / startup

--- src/CommandProcessor.rs
+++ src/CommandProcessor.rs
@@ -2,6 +2,8 @@ use crate::YamlProcessor;
 use std::collections::HashMap;
 use std::env;
 use std::process::{Command, Stdio, exit};
+use std::fmt;
+use std::io::Error;
 
 #[derive(Debug)]
 pub struct CommandBlock {
@@ -24,36 +26,113 @@ impl CommandBlock {
 
     /// Executes all commands of the command block
     // TODO -- BUG -- needs to return Result<(), Err>
-    pub fn execute(&self) {
+    pub fn execute(&self) -> Result<(), CommandError>{
         println!("Launching command {}", self.description);
         println!(
             "================================================================================"
         );
 
         for command in &self.commands {
-            CommandBlock::run_shell_command(&command);
+
+            // Execute this command and check for errors
+            match CommandBlock::run_shell_command(&command){
+                Err(err) => return Err(err),
+                Ok(()) => (),
+            }
         }
+
+        // All commands executed well
+        return Ok(());
     }
 
     /// Runs a given shell command in interactive mode
     /// It should never be called directly, thus is private
-    /// TODO -- handle exceptions
-    fn run_shell_command(command: &str) {
+    /// Returns the result of executing the command
+    fn run_shell_command(command: &str) -> Result<(), CommandError>{
         let user_env_vars: HashMap<String, String> = env::vars().collect();
-        let mut command_handle = Command::new("bash")
+        let command_handle = Command::new("bash")
             .env_clear()
             .envs(user_env_vars)
             .stdin(Stdio::inherit())
             .arg("-c")
             .arg(command)
-            .arg("-Syyu")
-            .spawn()
-            .expect(&("Failed to run command".to_owned() + command));
+            .arg("-Syyu") // TODO -- why this
+            .spawn();
+
+        let mut command_handle = match command_handle{
+            Ok(command_handle) => command_handle,
+            Err(err) => {
+                return Err(CommandError::new_not_started(err));
+            }
+        };
 
         // Wait for the command
-        command_handle.wait().unwrap();
+        match command_handle.wait(){
+            // We are not interested in the result of the execution, just that if has finished
+            Ok(_) => return Ok(()),
+
+            // An error has ocurred while executing the command
+            Err(err) => {
+                return Err(CommandError::new_exection_error(err));
+            }
+        }
     }
 }
+
+/// Types of error or launching a Command
+enum CommandErrorType{
+    /// The command was not able to run at first place
+    NotStarted,
+
+    /// The command was able to start, but had some failure during execution
+    ExecutionError,
+}
+
+/// Represents an error ocurred during command execution
+pub struct CommandError{
+    error_type: CommandErrorType,
+    description: String,
+}
+
+impl CommandError{
+    /// The command could not start its execution
+    pub fn new_not_started(error: std::io::Error) -> Self{
+        return Self{
+            error_type: CommandErrorType::NotStarted,
+            description: error.to_string(),
+        };
+    }
+
+    /// The command started properly, but had a problem during its execution
+    pub fn new_exection_error(error: std::io::Error) -> Self{
+        return Self{
+            error_type: CommandErrorType::ExecutionError,
+            description: error.to_string(),
+        };
+    }
+}
+
+impl fmt::Display for CommandError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.error_type{
+            CommandErrorType::NotStarted => {
+                let first_line = "[Error] Command could not start";
+                let second_line = format!("\tDescription: {}", self.description);
+                let msg = format!("{}\n{}", first_line, second_line);
+                return write!(f, "{}", msg);
+            }
+
+            CommandErrorType::ExecutionError => {
+                let first_line = "[Error] Command started but failed during execution";
+                let second_line = format!("\tDescription: {}", self.description);
+                let msg = format!("{}\n{}", first_line, second_line);
+                return write!(f, "{}", msg);
+            }
+        }
+    }
+}
+
+
 
 /// Handler to --shell cli argument
 /// Reads the shell yaml config file and executes commands described in the yaml file
