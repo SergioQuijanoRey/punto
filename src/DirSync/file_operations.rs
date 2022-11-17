2785e0087c656d1d8fe0b8d00d77f55931a89b0c
Sergio Quijano <sergiquijano@gmail.com>
2022-11-17 18:05:07 +0100

    feat, test #11: `copy_dir` now works with rsync. Tested some cases
    - Added some basic tests
    - ignore_files are neither implemented nor tested

--- src/DirSync/file_operations.rs
+++ src/DirSync/file_operations.rs
@@ -1,6 +1,8 @@
 /// Module to implement file operations such as copy files, copy dirs, create dirs, ...
 
-use std::{fs, path::Path, process::exit, thread, time};
+use std::{fs, path::Path, process::exit};
+
+use crate::SingleCommand;
 
 /// Creates recursively a dir if does not exist
 // TODO -- not sure if it is creating dir recursively if depth is greater than 2
@@ -18,33 +20,69 @@ pub fn create_dir_if_not_exists(path: &str) {
     }
 }
 
-/// Copies one dir to other recursively
-// TODO -- BUG -- ignore_files behaviour is not correct
-//      -- We are deleting ignore_files instead of actually ignoring them;
-pub fn copy_dir_recursively(from: &str, to: &str, ignore_files: &Vec<String>) {
-    // Options that we need to pass to copy_dir_advanced
-    let overwrite_all = true;
-    let overwrite_if_newer = true;
-    let overwrite_if_size_differs = true;
-    let include_filters : Vec<String> = Vec::new();
+#[derive(Debug)]
+pub enum FileOperationError{
 
-    let copy_result = dircpy::copy_dir_advanced(
-        from,
-        to,
-        overwrite_all,
-        overwrite_if_newer,
-        overwrite_if_size_differs,
-        ignore_files.to_vec(),
-        include_filters
+    // Error ocurred while copying one dir to another dir
+    DirCopyError(String),
+}
+
+/// Gets a path and adds a last "/" if it is not present
+/// i.e. some/path -> some/path/
+/// i.e other/path/ -> do nothing
+fn add_last_slash_to_path(path: &str) -> String{
+    let last_char = path.chars().last().unwrap();
+
+    if last_char == '/' {
+        return path.to_string();
+    }
+
+
+    let mut transformted_path = path.to_string();
+    transformted_path.push('/');
+
+    return transformted_path;
+}
+
+/// Copies one dir to other recursively
+/// Files in the `to` dir that are not present in the `from` dir are preserved
+// TODO -- we're using rsync to do this, move that to native rust code
+// TODO -- needs a lots of testing
+// TODO -- BUG -- we are not ignoring files
+pub fn copy_dir_recursively(from: &str, to: &str, ignore_files: &Vec<String>) -> Result<(), FileOperationError>{
+
+    // For using rsync, last char in the paths must be /
+    // So make some checks and do the conversion if they fail
+    // i.e. some/path -> some/path/
+    let from = add_last_slash_to_path(&from);
+    let to = add_last_slash_to_path(&to);
+
+    // Build a bash command based on rsync to perform the operation
+    let command_content = format!("rsync -zaP {from} {to}");
+    println!("TODO -- command is {command_content}");
+    let quiet = false;
+    let sudo = false;
+    let command = SingleCommand::SingleCommand::new(
+        command_content, quiet, sudo,
     );
 
-    match copy_result{
-        Ok(()) => (),
-        Err(err) => {
-            eprintln!("Error copying dir {} to {}", from, to);
-            eprintln!("Err Code: {}", err);
-        }
-    }
+    // Check that the command is valid
+    let command = match command{
+        Ok(result) => result,
+        Err(err) => return Err(FileOperationError::DirCopyError(format!("rsync command creation failed: {err:?}"))),
+    };
+
+    // Run the command
+    let command_result = command.run();
+
+    // Check for errors
+    // Translate SingleCommandError to a higher level of abstraction error
+    let operation_result = match command_result {
+        Ok(_) => Ok(()),
+        Err(err) => Err(FileOperationError::DirCopyError(format!("Rsync error was {err:?}"))),
+    };
+
+    return operation_result;
 }
 
 /// Syncs two dirs.
@@ -133,7 +171,12 @@ pub fn join_two_paths(first: &str, second: &str) -> String{
 
 #[cfg(test)]
 mod tests {
-    use super::join_two_paths;
+    use std::fs;
+    use std::path::Path;
+
+    use crate::DirSync::file_operations::add_last_slash_to_path;
+
+    use super::{join_two_paths, copy_dir_recursively};
 
     #[test]
     fn test_join_two_paths_basic() {
@@ -157,5 +200,70 @@ mod tests {
         let computed = join_two_paths("testing/", "this/");
         let expected = "testing/this/";
         assert_eq!(expected, computed);
+    }
+
+    #[test]
+    fn test_add_last_slash_to_path(){
+        let original_path = "some/path";
+        let transformted_path = add_last_slash_to_path(original_path);
+        assert_eq!(transformted_path, "some/path/", "add_last_slash_to_path did not added last slash");
+
+
+        let original_path = "some/path/";
+        let transformted_path = add_last_slash_to_path(original_path);
+        assert_eq!(transformted_path, "some/path/", "add_last_slash_to_path changed a path that was correct at first");
+    }
+
+    /// A lot of tests need to work in top a file hierarchy structure
+    /// So with this function we can create a basic structure
+    fn create_basic_file_structure() -> Option<()>{
+        fs::create_dir("./dir_tests").ok()?;
+        fs::create_dir("./dir_tests/src").ok()?;
+        fs::create_dir("./dir_tests/test").ok()?;
+        fs::File::create("./dir_tests/src/first.rs").ok()?;
+        fs::File::create("./dir_tests/src/second.rs").ok()?;
+        fs::File::create("./dir_tests/src/third.rs").ok()?;
+        fs::File::create("./dir_tests/test/first_test.rs").ok()?;
+        fs::File::create("./dir_tests/test/second_test.rs").ok()?;
+
+        return Some(());
+    }
+
+    /// Remove the basic file structure created with `create_basic_file_structure`
+    fn remove_basic_file_structure() -> Option<()>{
+        fs::remove_dir_all("./dir_tests").ok()?;
+
+        return Some(());
+    }
+
+    #[test]
+    fn test_copy_dir_recursively_base_case(){
+
+        // Start creating a basic file structure
+        // If a test fails, this structure might be already created, so delete if first
+        remove_basic_file_structure();
+        create_basic_file_structure().expect("Could not create basic file structure for the test");
+
+        // Copy now to another path
+        let from = "./dir_tests";
+        let to = "./dir_tests/pruebas";
+        let ignore_files = vec![];
+        copy_dir_recursively(from, to, &ignore_files).expect("Copy operation failed to run");
+
+        // Make some checks about the dirs
+        assert!(Path::new("./dir_tests/pruebas/").exists(), "New dir hierarchy was not created properly");
+        assert!(Path::new("./dir_tests/pruebas/src").exists(), "New dir hierarchy was not created properly");
+        assert!(Path::new("./dir_tests/pruebas/test").exists(), "New dir hierarchy was not created properly");
+
+        // Now check the paths
+        assert!(Path::new("./dir_tests/pruebas/src/first.rs").exists(), "New dir hierarchy was not created properly");
+        assert!(Path::new("./dir_tests/pruebas/src/second.rs").exists(), "New dir hierarchy was not created properly");
+        assert!(Path::new("./dir_tests/pruebas/src/third.rs").exists(), "New dir hierarchy was not created properly");
+
+        assert!(Path::new("./dir_tests/pruebas/test/first_test.rs").exists(), "New dir hierarchy was not created properly");
+        assert!(Path::new("./dir_tests/pruebas/test/second_test.rs").exists(), "New dir hierarchy was not created properly");
+
+        // Now, remove the file hierarchy created
+        remove_basic_file_structure();
     }
 }
